1bd35f9d2a884804171df283ec08d1531d7833d8
thales.morais21 <thales.morais21@gmail.com>
2018-05-13 16:16:36 -0300

    nada

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -29,16 +29,19 @@ class BotController extends Controller
     {
         $sender = new SenderRequest;
         $senderId = $sender->getSenderId();
+        $postback = $sender->getPostback();
         $message = new ButtonsTemplate($senderId);
-        $callSendApi = new CallSendApi('EAAFP19VlnrIBADsfumDncgn5YWXojvrNmpZBf4OxGVcLsRQOTcJs040a26SsLTIdU1crz1wqa668ZBrQgZBgZBjQJmAO0bfMszqfwP6ABAn9umymH6OPlYeEFvrSzF5mO7f941cCIvYl3fQ3xOUqRy3sZBl9sPFLpXl8SAJzjNwZDZD');
+
 
         $bot = Solid::factory();
         Solid::pageAccessToken('EAAFP19VlnrIBADsfumDncgn5YWXojvrNmpZBf4OxGVcLsRQOTcJs040a26SsLTIdU1crz1wqa668ZBrQgZBgZBjQJmAO0bfMszqfwP6ABAn9umymH6OPlYeEFvrSzF5mO7f941cCIvYl3fQ3xOUqRy3sZBl9sPFLpXl8SAJzjNwZDZD');
         Solid::setSender($senderId);
-        $message = new ButtonsTemplate($senderId);
+        $callSendApi = new CallSendApi('EAAFP19VlnrIBADsfumDncgn5YWXojvrNmpZBf4OxGVcLsRQOTcJs040a26SsLTIdU1crz1wqa668ZBrQgZBgZBjQJmAO0bfMszqfwP6ABAn9umymH6OPlYeEFvrSzF5mO7f941cCIvYl3fQ3xOUqRy3sZBl9sPFLpXl8SAJzjNwZDZD');
 
 
-        $bot->message('text', 'ola eu sou o VIMIT, para começarmos poderia me dizer seu nome ? ');
+
+        $bot->message('text', 'ola eu sou o VIMIT, vamo começar nossa aula');
+
 
 
 
