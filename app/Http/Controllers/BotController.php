eba6f27927ad0c9f9606b536b1b9652ed9c1ecdc
thales.morais21 <thales.morais21@gmail.com>
2018-05-13 16:22:43 -0300

    nada

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -41,6 +41,12 @@ class BotController extends Controller
 
 
         $bot->message('text', 'ola eu sou o VIMIT, vamo começar nossa aula');
+        if($postback === "iniciar"){
+                $bot->message("text", 'Sobre o que voce quer abrender CIFRAS, TABLATURAS, RITMOS ?');
+        }else{
+            $message->add(new Button('postback', 'Iniciar aula', 'iniciar'));
+        }
+
 
 
 
