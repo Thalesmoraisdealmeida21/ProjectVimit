358b85966fbbc213d8d52e2819c51b69763821c9
thales.morais21 <thales.morais21@gmail.com>
2018-05-13 17:16:54 -0300

    First commit

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -14,11 +14,12 @@ use App\usuario;
 class BotController extends Controller
 {
     private $first = 0;
+
     public function subscribe()
     {
         $subscribe = (new WebHook)->check('a1sd65162sad16as6das1d1as66asd1sad1611');
         if(!$subscribe){
-            abort(403, 'Deu ruim !!!');
+            abort(403, 'Unauthorized action');
         }
 
         return $subscribe;
@@ -37,12 +38,22 @@ class BotController extends Controller
         Solid::pageAccessToken('EAAFP19VlnrIBADsfumDncgn5YWXojvrNmpZBf4OxGVcLsRQOTcJs040a26SsLTIdU1crz1wqa668ZBrQgZBgZBjQJmAO0bfMszqfwP6ABAn9umymH6OPlYeEFvrSzF5mO7f941cCIvYl3fQ3xOUqRy3sZBl9sPFLpXl8SAJzjNwZDZD');
         Solid::setSender($senderId);
         $callSendApi = new CallSendApi('EAAFP19VlnrIBADsfumDncgn5YWXojvrNmpZBf4OxGVcLsRQOTcJs040a26SsLTIdU1crz1wqa668ZBrQgZBgZBjQJmAO0bfMszqfwP6ABAn9umymH6OPlYeEFvrSzF5mO7f941cCIvYl3fQ3xOUqRy3sZBl9sPFLpXl8SAJzjNwZDZD');
-
-        $bot->message('text','Certo, mas antes de prosseguirmo para que eu possa lhe ajudar melhor poderia me dizer seu nome ?');
-
+        $bot->message($postback);
 
 
 
+        if($postback === 'iniciar'){
+            $usuario = new usuario\Usuario();
+            $bot->message('text','Certo, mas antes de prosseguirmo para que eu possa lhe ajudar melhor poderia me dizer seu nome ?');
+            $usuario->setIdMensagemAtu('Certo, mas antes de prosseguirmo para que eu possa lhe ajudar melhor poderia me dizer seu nome ?');
+        }else {
+            $message->add(new Button('postback', 'Pronto', 'iniciar'));
+            $callSendApi->make($message->message('Ola eu sou o VIMIT, seu professor virtual de musicapronto para começar nossa aula  ?'));
+        }
+        if($usuario->getIdMensagemAtu() === 'Certo, mas antes de prosseguirmos para que eu possa lhe ajudar melhor poderia me dizer seu nome ?'){
+            $usuario->setName($sender->getMessage());
+            $bot->message('text', 'Ola, '. $usuario->getName(). 'Sobre o que você desejha saber ?');
+        }
 
 
 
