28b1b1dcc8e22854a533a75dd4418da641629a8d
thales.morais21 <thales.morais21@gmail.com>
2018-05-20 15:42:33 -0300

    adicionado todas as cifras

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,51 +2,6 @@
 
 namespace App\Http\Controllers;
 
-$lembranças =  'Intro: F  C  Bb  C  F
-
-F                              C
-Quando as almas perdidas se encontram
-                       F
-Machucadas pelo desprazer
-    Bb      F                C
-Um aceno um riso apenas
-                       F
-Da vontade da gente viver
-                            C
-São os velhos mistérios da vida
-                         Bb
-Rebenquiados pelo dia-a-dia
-
-      F                     C
-Já cansados de tanta tristeza
-                          F
-Vão em busca de nova alegria (2x)
-
-Solo: F  C  Bb  C  F
-
-F                          C
-E ao morrer desta tarde morena
-                           F
-Quando o sol despacito se vai
-      Bb            F           C
-As lembranças tranqueiam com as águas
-                        F
-Passageiras do rio Uruguai
-                          C
-E as guitarras eternas cigarras
-                             Bb
-Entre as flores dos velhos ipês
-
-        F                    C
-Sempre vivas dormidas se acordam
-                          F
-Na lembrança da primeira vez
-
-        Bb                 C
-Sempre vivas dormidas se acordam
-                          F
-Na lembrança da primeira vez.';
-
 
 use CodeBot\CallSendApi;
 use CodeBot\Element\Button;
@@ -91,9 +46,7 @@ class BotController extends Controller
 
            /* Possibilidades de CIFRAS*/
             if($mensagem1 === 'LEMBRANÇAS'){
-                $bot->message('text', 'Intro: F  C  Bb  C  F
-
-F                              C
+                $bot->message('text', 'F                              C
 Quando as almas perdidas se encontram
                        F
 Machucadas pelo desprazer
@@ -136,9 +89,7 @@ Sempre vivas dormidas se acordam
                           F
 Na lembrança da primeira vez.');
             } else if($mensagem1 === 'CHALANA'){
-                $bot->message('text', 'Intro:
-
-Primeira Parte:
+                $bot->message('text', 'Primeira Parte:
     D
 Lá vai uma chalana
      A7(13)   D
@@ -200,17 +151,9 @@ Tu aumentas minha dor
 Nessas águas tão serenas
                   D
 Vai levando meu amor
-
-Solo:
-
-(Primeira Parte)
-
-(Refrão)');
+');
             } else if($mensagem1 === 'LENHA'){
-                $bot->message('text', '(intro 2x) C   G   F   C
-
-              G
-Eu não sei dizer,
+                $bot->message('text', 'Eu não sei dizer,
               F
 O que quer dizer,
              C
@@ -280,7 +223,10 @@ Você traz a lenha
              C
 Pro meu fogo acender...');
             } else if($mensagem1 === 'FLOR'){
-                $bot->message('text', '  F#m       D5(9)
+                $bot->message('text', 'Capotraste na 2ª casa
+Intro:  F#m
+
+  F#m       D5(9)
 Flor, Oh Uh Oh!
       A5(9)      E
 Oh Uh Oh!  Oh Uh Oh!
@@ -363,8 +309,7 @@ A                    E
       Bm                    D5(9)  E
 Te regando flor cê vai me amar
 
-Intro: ( F#m )
-');
+Intro: ( F#m )');
             } else if($mensagem1 === 'DIGA A ELA'){
                 $bot->message('text', 'G                    Am D
    Diga a ela que você me viu
@@ -372,6 +317,7 @@ G               Am   D
    Que eu parecia muito bem
 G                 Am             D
    Apesar de tantas noites vazias
+
 Em                  A      C
    Tantas madrugadas vendo tv
       G           F      Em
@@ -390,11 +336,11 @@ C        G            D  Em
    Diga a ela que me viu sozinho
 C          G      D           Em
    Diga que ela sabe onde eu estou
-C        G       D       Em   
+C        G       D       Em
    Diga a ela que me viu sozinho
          C     Em         A
    Ela sabe onde eu estou
-Intro 
+Intro
 
 G                  Am     D
    Diga a ela que me viu na rua
@@ -410,18 +356,294 @@ C        G            D  Em
    Diga a ela que me viu sozinho
 C          G      D           Em
    Diga que ela sabe onde eu estou
-C        G       D       Em   
+C        G       D       Em
    Diga a ela que me viu sozinho
          C     Em         A
    Ela sabe onde eu estou');
             }else if($mensagem1 === 'TERTULIA'){
-                $bot->message('text', 'CIFRA TERTULIA');
+                $bot->message('text', '        C            G7
+(Uma chamarra uma fogueira
+        E           F7M
+Uma chinoca uma chaleira
+       Ebº             C
+Uma saudade, um mate amargo
+        G7                 C     (bis)
+E a peonada repassando o trago
+                      G
+Noite cheirando a querência
+                      C
+Das tertúlias do meu pago)
+
+
+(também pode ser feito da maneira mostrada abaixo)
+
+  Noite  cheiran-do a que-rência  nas ter-túlias do meu pago
+|-0--------------------------------------------------1------|
+|----1----3-------1-------------------------------1--3---1--|
+|------------4-------2----4-------2-------0-------2--2---0--|
+|----------------------------5-------4-------2----3--0---2--|
+|-3---------------------------------------3--------------3--|
+|---------3-------1-------3-------1---------------1---------|
+
+
+                     G7                       C
+Tertúlia é o eco das vozes perdidas no campo afora
+                  G7                         C
+Cantiga brotando livre novo prenúncio de aurora
+   E              F       Ebº               C
+É rima sem compromisso julgamento ou castração
+                    G7                    C
+Onde se marca o compasso no bater do coração
+
+                     G7                     C
+É o batismo dos sem nome rodeio dos desgarrados
+                    G7                       C
+Grito de alerta do pampa tribuna dos injustiçados
+    E                 F         Ebº            C
+Tertúlia é o canto sonoro sem fronteira ou aramado
+                    G7                    C
+Onde o violão e o poeta podem chorar abraçados
+
+(intro)');
             } else if($mensagem1 === 'LA NA FRONTEIRA'){
-                $bot->message('text', 'CIFRA LA NA FRONTEIRA');
+                $bot->message('text', '(intro) D A7 D
+Lá donde o campo enfrena o dia, abrindo o peito
+                                  A7
+No velho jeito de tirar zebú da grota
+          Em            Em/D        A/C#
+Se ata espora pra um torrão de fundamento
+A7                        D
+Passando um tento, embaixo do taco da bota
+
+
+Lá donde o touro mais veiaco tem costeio
+            F#m                     Bm
+Um par de arreio é ferramenta de valor
+        A7               G        D
+A vaca xucra esconde a cria na macega
+                 A7                       D    (bis)
+E cavalhada não nega, que por lá hay domador
+
+              D
+/Lá donde as penas se transformam em melodias
+                                        A7
+Na campeira sinfonia de coscorra e nazarenas
+         Em             Em/D       A/C#
+Almas antigas rondam galpões nas estâncias
+                        A7                           D
+Pois são grandes as distâncias e as saudades tão pequenas)
+
+Lá donde ainda ecoa forte um venha, venha
+            F#m                     Bm
+Chamando a tropa, no reponte das auroras
+        A7           G              D
+A bagualada segue atrás da égua madrinha
+                     A7                         D    (bis)
+Na velha estrada da linha, serpenteando tempo afora
+
+           A7                        D
+Lá na fronteira, os tajãs por contingêngia
+                   A7                             D
+Contrabandeiam querência, ora pra um lado ora pra outro
+            A7                          D
+Se ganha a vida a casco e braço nos varzedos
+            A7                          D
+Se aprende cedo a ensiná a lida pra um potro
+
+          F#7                          Bm
+Lá na fronteira, na amplidão das invernadas
+                    E7                           A7
+Se termina a campereada, quando o sol apaga as brasas
+          G                        D
+Então se volta, a trotezito, assoviando
+                      A7                          D    (bis)
+Pra matear junto da china num jardim defronte as casa
+
+( F#7 Bm E7 A7 G D A7 D G D A7 D )
+
+          G                        D
+Então se volta, a trotezito, assoviando
+                      A7                          D    (bis)
+Pra matear junto da china num jardim defronte as casa');
             }else if($mensagem1 === 'ERA UMA VEZ'){
-                $bot->message('text', 'CIFRA ERA UMA VEZ');
+                $bot->message('text', 'Capotraste na 3ª casa
+[Intro] C  C4
+
+[Primeira Parte – Primeira Estrofe]
+
+         C
+Era uma vez
+                           C4
+O dia em que todo dia era bom
+      C
+Delicioso gosto e o bom gosto
+                                C4
+Das nuvens serem feitas de algodão
+
+[Primeira Parte – Segunda Estrofe]
+
+          G5
+Dava pra ser herói
+                                    Am7
+No mesmo dia em que escolhia ser vilão
+      G5
+E acabava tudo em lanche
+          F9
+Um banho quente
+                  C
+E talvez um arranhão
+
+[Segunda Parte – Primeira Estrofe]
+
+          C
+Dava pra ver
+  C9                 F9
+A ingenuidade, a inocência
+
+Cantando no tom
+            C                       C9
+Milhões de mundos e universos tão reais
+                       F9
+Quanto a nossa imaginação
+
+[Segunda Parte – Segunda Estrofe]
+
+            G5
+Bastava um colo, um carinho
+                              Am7
+E o remédio era beijo e proteção
+         G5                       F9
+Tudo voltava a ser novo no outro dia
+                   C
+Sem muita preocupação
+
+[Refrão]
+
+                     F9    G5
+É que a gente quer crescer
+                           Am7
+E, quando cresce, quer voltar do início
+                    F9
+Porque um joelho ralado
+         G5                      Am7
+Dói bem menos que um coração partido
+                    
+                     F9    G5
+É que a gente quer crescer
+                           Am7
+E, quando cresce, quer voltar do início
+                    F9
+Porque um joelho ralado
+         G5                      Am7
+Dói bem menos que um coração partido
+
+[Terceira Parte – Primeira Estrofe]
+
+          C
+Dá pra viver
+
+Mesmo depois de descobrir 
+       C4
+Que o mundo ficou mau
+          C
+É só não permitir 
+
+Que a maldade do mundo
+              C4
+Te pareça normal
+
+[Terceira Parte – Segunda Estrofe]
+
+            G5
+Pra não perder a magia de acreditar
+                Am7
+Na felicidade real
+        G5                    F9
+E entender que ela mora no caminho
+            C
+E não no final
+
+[Refrão]
+
+                     F9    G5
+É que a gente quer crescer
+                           Am7
+E, quando cresce, quer voltar do início
+                    F9
+Porque um joelho ralado
+         G5                      Am7
+Dói bem menos que um coração partido
+                    
+                     F9    G5
+É que a gente quer crescer
+                           Am7
+E, quando cresce, quer voltar do início
+                    F9
+Porque um joelho ralado
+         G5                      Am7
+Dói bem menos que um coração partido
+         C9
+Era uma vez
+');
             }else if($mensagem1 === 'BELLA CIAO'){
-                $bot->message('text', 'CIFRA BELLA CIAO');
+                $bot->message('text', 'Em
+Una mattina mi son svegliato
+
+O bella ciao, bella ciao, bella ciao ciao ciao
+        Am               Em
+Una mattina mi son svegliato
+          B7         Em
+Eo ho trovato l\'invasor
+
+        Em
+O partigiano porta mi via
+
+O bella ciao, bella ciao, bella ciao ciao ciao
+        Am             Em
+O partigiano porta mi via
+        B7         Em
+Che mi sento di morir
+
+          Em
+E se io muoio da partigiano
+
+O bella ciao, bella ciao, bella ciao ciao ciao
+         Am             Em
+E se io muoio da partigiano
+       B7         Em
+Tu mi devi seppellir
+
+         Em
+E seppellire lassù in montagna
+
+O bella ciao, bella ciao, bella ciao ciao ciao
+         Am               Em
+E seppellire lassù in montagna
+        B7               Em
+Sotto l\'ombra di un bel fior
+
+Em
+E le genti che passeranno
+
+O bella ciao, bella ciao, bella ciao ciao ciao
+      Am             Em
+E le genti che passeranno
+      B7              Em
+Mi diranno: "Che bel fior"
+
+             Em
+È questo il fiore del partigiano
+
+O bella ciao, bella ciao, bella ciao ciao ciao
+             Am             Em
+È questo il fiore del partigiano
+       B7          Em
+Morto per la libertà
+
+             C              Em
+È questo il fiore del partigiano
+       D          Em
+Morto per la libertà');
             }else{
                 if($postback === 'iniciar'){
                     $bot->message("text", 'Ok vamos começar');
